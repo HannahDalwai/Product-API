e5b5c8b35ba2cb1f57aaaaba436d08fab7d9407e
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-05 11:53:15 +0200

    new thing

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-fetch("https://fakestoreapi.com/products")
-  .then((response) => response.json())
-  .then((data) => {
-      console.log(data);
-    data.forEach(product => {
-        document.getElementById("product").innerHTML += `
-        <img src="${product.image}"> 
-        <h2>${product.price}</h2>
-        <h3>${product.description}</h3>
-
-     ` });
+fetch("https://pokeapi.co/api/v2/pokemon")
+.then(res => res.json())
+.then ((pokemonList) => {
+  console.log(pokemonList);
+  pokemonList.results.forEach(pokemon => {
+    document.querySelector("#list").innerHTML +=`
+    <button onclick="getData('pokemon.url')">${pokemon.name}</button>
+    `
+  })
 });
+
+
+function getData(url){
+fetch("https://pokeapi.co/api/v2/pokemon/charizard")
+.then((res)=>res.json())
+.then((pokemon)=>{
+  console.log(pokemon);
+  document.querySelector(#results).innerHTML = `
+  <img scr= "${pokemon.spirtes.front_default}" alt="" >
+  <h1>${pokemon.name} </h1>
+  <h4>Ability : ${pokemon.abilities[0].abilities.name}</h4>
+  `;
+}
+      });
