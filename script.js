8a0d3be0e4c0b27a236f97e1debec335b2257624
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-05 12:27:17 +0200

    cri

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 fetch("https://pokeapi.co/api/v2/pokemon")
 .then(res => res.json())
-.then ((pokemonList) => {
+.then((pokemonList) => {
   console.log(pokemonList);
   pokemonList.results.forEach(pokemon => {
     document.querySelector("#list").innerHTML +=`
-    <button onclick="getData('pokemon.url')">${pokemon.name}</button>
+    <button onclick="getData('${ pokemon.url }')">${pokemon.name}</button>
     `
   })
 });
 
 
 function getData(url){
-fetch("https://pokeapi.co/api/v2/pokemon/charizard")
+fetch(url)
 .then((res)=>res.json())
 .then((pokemon)=>{
   console.log(pokemon);
-  document.querySelector(#results).innerHTML = `
-  <img scr= "${pokemon.spirtes.front_default}" alt="" >
+  document.querySelector("#results").innerHTML = `
+  <img scr= "${pokemon.sprites.front_default}" alt="" >
   <h1>${pokemon.name} </h1>
-  <h4>Ability : ${pokemon.abilities[0].abilities.name}</h4>
+  <h4>Ability : ${pokemon.abilities[0].ability.name}</h4>
   `;
-}
-      });
+})
+      };
+  
