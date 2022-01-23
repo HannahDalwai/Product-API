73b59117af7e5ea1d0396eba1914c8a9d59fed8b
HannahDalwai <hannahdalwai4@gmail.com>
2022-01-23 14:46:54 -0800

    styling

--- script.js
+++ script.js
@@ -4,27 +4,41 @@ fetch("https://pokeapi.co/api/v2/pokemon")
   console.log(pokemonList);
   pokemonList.results.forEach(pokemon => {
     document.querySelector("#list").innerHTML +=`
-    <button id="btn" onclick="getData('${ pokemon.url }')">${pokemon.name}</button>
+<button id="btn" onclick="getData('${ pokemon.url }')" class="col btn btn-cool-blues">${pokemon.name}</button>
     `
   })
 });
 
-
+// READ
 function getData(url){
 fetch(url)
 .then((res)=>res.json())
 .then((pokemon)=>{
   console.log(pokemon);
   document.querySelector("#results").innerHTML = `
-  <img scr= "${pokemon.sprites.front_default}" alt="" >
-  <h1>${pokemon.name} </h1>
-  <h4> Ability : ${pokemon.abilities[0].ability.name} </h4>
-  <h4> Weight : ${pokemon.weight} </h4>
-  <h4> Height : ${pokemon.height} </h4>
+  <div class="card" style="width: 18rem;">
+  <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${pokemon.name}</h5>
+    <p class="card-text">${pokemon.height}</p>
+    <p class="card-text">${pokemon.weight}</p>
+    <p class="card-text">${pokemon.abilities[0].ability.name}</p>
+  </div>
+</div>
+
+
 
 
 
   `;
 })
       };
-  
+      getData("https://pokeapi.co/api/v2/pokemon/pikachu");
+
+      // SEARCH 
+      // function searchPokemon():void
+      // function searchPokemon(){
+      //   const searchTerm = document.querySelector("#search").value
+      //   const url = "https//pokeapi.co/api/vs/pokemon/" + searchTerm;
+      //   getPokemoninfo(url);
+      // }
